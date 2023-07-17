7702f5e154b9dcb48f8c31b7fb5e7b4b0bc26ac5
Nafid-Zanis <nafidzanis6@gmail.com>
2023-07-17 09:52:57 +0700

    First Commit

---
+++ app/Views/layout.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>BAB 3</title>
+    <meta charset="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" href="assets/CSS/style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+    </style>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Styling CSS</title>
+</head>
+
+<body>
+    <!-- Navigasi Anda -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-5 sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon active"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/profile">Halaman Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/experience">Halaman Pengalaman</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <h1 class="text-center">Muhammad Nafid Zanis</h1>
+    <p class="text-center"><i>Halo, nama saya Muhammad Nafid Zanis. Saya merupakan Web Developer dari </i><b>PT. Al - Aqso</b></p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#0099ff" fill-opacity="1" d="M0,0L18.5,16C36.9,32,74,64,111,90.7C147.7,117,185,139,222,165.3C258.5,192,295,224,332,208C369.2,192,406,128,443,106.7C480,85,517,107,554,133.3C590.8,160,628,192,665,186.7C701.5,181,738,139,775,101.3C812.3,64,849,32,886,42.7C923.1,53,960,107,997,106.7C1033.8,107,1071,53,1108,64C1144.6,75,1182,149,1218,165.3C1255.4,181,1292,139,1329,106.7C1366.2,75,1403,53,1422,42.7L1440,32L1440,0L1421.5,0C1403.1,0,1366,0,1329,0C1292.3,0,1255,0,1218,0C1181.5,0,1145,0,1108,0C1070.8,0,1034,0,997,0C960,0,923,0,886,0C849.2,0,812,0,775,0C738.5,0,702,0,665,0C627.7,0,591,0,554,0C516.9,0,480,0,443,0C406.2,0,369,0,332,0C295.4,0,258,0,222,0C184.6,0,148,0,111,0C73.8,0,37,0,18,0L0,0Z"></path>
+    </svg>
+    <!-- Konten Utama akan dirender di sini -->
+    <h1 class="text-center"><b>About me</b></h1>
+    <p class="text-center">Saya mempunyai bakat di beberapa bidang bahasa terutama framework dalam pengembangan Web Development</p>
+    <br>
+    <div class="row">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    UI / UX Design
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Medan / 2019</li>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis odit doloribus, at qui ipsam voluptatibus repellat accusamus doloremque saepe numquam.</p>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Web Design
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Kudus / 2020</li>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, necessitatibus quisquam cum laboriosam exercitationem libero quidem? Eaque rem dolore aliquam!</p>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Digital Marketing
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Jakarta / 2021</li>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias minus asperiores culpa quia eos odit.</p>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Freelance Web Design
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Bali / 2022</li>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dignissimos earum quo ullam quos eaque veritatis tempore? </p>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
